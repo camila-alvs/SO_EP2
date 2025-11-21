a89c46957744f3a60d4756565d2c6097a3e93ed0
Camila <camila.atsilva@gmail.com>
2025-11-21 01:37:29 -0300

    Classe DataBase criada

---
+++ src/DataBase.java
@@ -0,0 +1,42 @@
+import java.io.IOException;
+import java.nio.file.Paths;
+import java.nio.file.Files;
+import java.util.ArrayList;
+import java.util.List;
+
+public class DataBase {
+    public static String[] database;
+    public static boolean lock; // Se true base está bloqueada, se false base está livre
+
+    static {
+        database = null;
+        lock = false;
+    }
+
+    public static void populateDatabase(String filePath) {
+        // Lendo o arquivo
+        List<String> bdFileContents = new ArrayList<String>();
+        try {
+            bdFileContents = Files.readAllLines(Paths.get(filePath));
+        } catch (IOException error) {
+            System.out.println("Erro lendo a base: " + error);
+        }
+
+        // Armazenando na variável database
+        database = new String[bdFileContents.size()];
+        for(int i=0; i<bdFileContents.size(); i++) 
+            database[i] = bdFileContents.get(i);
+        return;
+    }
+
+    public static void acquireLock() {
+        lock = true;
+    }
+    public static void releaseLock() {
+        lock = false;
+    }
+
+    public static void cleanUp() {
+        database = null;
+    }
+}

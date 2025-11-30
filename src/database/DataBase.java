be5ce576a20932b868c0da2fbe4fbef6e34dd080
Camila <camila.atsilva@gmail.com>
2025-11-30 14:52:42 -0300

    Tirei o lock do database, agora sim versão final

--- src/database/DataBase.java
+++ src/database/DataBase.java
@@ -8,7 +8,6 @@ import java.util.List;
 
 public class DataBase {
     public static String[] database;
-    public static boolean lock; // Se true base está bloqueada, se false base está livre
 
     static {
         database = null;
@@ -29,13 +28,6 @@ public class DataBase {
         for(int i=0; i<bdFileContents.size(); i++) 
             database[i] = bdFileContents.get(i);
         return;
-    }
-
-    public static void acquireLock() {
-        lock = true;
-    }
-    public static void releaseLock() {
-        lock = false;
     }
 
     public static void cleanUp() {
